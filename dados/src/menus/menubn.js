a0b87b0af43d11d68a5da0efcab0b12eea747f18
Enderson Evemy <endersonevemy4@gmail.com>
2026-05-21 23:31:55 -0400

    Update menubn.js

--- dados/src/menus/menubn.js
+++ dados/src/menus/menubn.js
@@ -10,7 +10,6 @@ export default async function menubn(prefix, botName = "MeuBot", userName = "Usu
     phrasesMenuTitle = "💬 FRASES & TEXTOS 📜",
     interactionsMenuTitle = "💬 INTERAÇÕES SOCIAIS 🤝",
     relationshipMenuTitle = "💞 RELACIONAMENTOS ❤️",
-    hotInteractionsMenuTitle = '🔥 INTERAÇÕES "PICANTES" 😏',
     maleFunMenuTitle = "🎯 BRINCADEIRAS MASCULINAS 🔥",
     femaleFunMenuTitle = "💅 BRINCADEIRAS FEMININAS 👸",
     maleRanksMenuTitle = "🏆 RANKINGS MASCULINOS 👑",
@@ -84,23 +83,7 @@ ${middleBorder}${menuItemIcon}${prefix}mata
 ${middleBorder}${menuItemIcon}${prefix}matar
 ${middleBorder}${menuItemIcon}${prefix}cafune
 ${bottomBorder}
-`;
-    if (!isLiteMode) {
-        menuContent += `
-${menuTopBorder}${separatorIcon} *${hotInteractionsMenuTitle}*
-${middleBorder}
-${middleBorder}${menuItemIcon}${prefix}surubao
-${middleBorder}${menuItemIcon}${prefix}sexo
-${middleBorder}${menuItemIcon}${prefix}beijob
-${middleBorder}${menuItemIcon}${prefix}beijarb
-${middleBorder}${menuItemIcon}${prefix}tapar
-${middleBorder}${menuItemIcon}${prefix}goza
-${middleBorder}${menuItemIcon}${prefix}gozar
-${middleBorder}${menuItemIcon}${prefix}mamar
-${middleBorder}${menuItemIcon}${prefix}mamada
-${bottomBorder}
-`;
-    }
+
     menuContent += `
 ${menuTopBorder}${separatorIcon} *${maleFunMenuTitle}*
 ${middleBorder}
