844d1fdc4d9792ee2ecc419a1fd2b885780d8b81
Enderson Evemy <endersonevemy4@gmail.com>
2026-05-29 00:34:03 -0400

    fix: remover menulogos do menu principal

--- dados/src/menus/menu.js
+++ dados/src/menus/menu.js
@@ -14,7 +14,7 @@ ${menuTopBorder}${separatorIcon} *MENU PRINCIPAL*
 ${middleBorder}
 ${middleBorder}${menuItemIcon}${prefix}menuia
 ${middleBorder}${menuItemIcon}${prefix}menudown
-${middleBorder}${menuItemIcon}${prefix}menulogos
+${middleBorder}${menuItemIcon}${prefix}menuedits
 ${middleBorder}${menuItemIcon}${prefix}menuadm
 ${middleBorder}${menuItemIcon}${prefix}menubn
 ${middleBorder}${menuItemIcon}${prefix}menudono
